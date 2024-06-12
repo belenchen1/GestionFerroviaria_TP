2df27df53a1f7500633d9495164b246eddd4c37a
Belén Chen <belendchen@icloud.com>
2024-06-12 19:09:05 -0300

    .

--- src/main_alu.py
+++ src/main_alu.py
@@ -7,12 +7,11 @@ import math
 def setear_grafo(trenes):
    
    cabeceras = tuple(trenes['stations'])
-   edges = []
    R = nx.DiGraph()
-   
+
    for s in trenes['services'].values():
       # agrego nodos, en principio con demanda 0
-      for i in range(2): # 0 <-> type D   ^   1 <-> type A
+      for i in range(2): # 0 <-> type D (departure)  ^  1 <-> type A (arrival)
          nodo_id = (s['stops'][i]['time'], s['stops'][i]['station'])
          if nodo_id not in R.nodes():
             R.add_node(nodo_id, demand=0)
@@ -21,26 +20,25 @@ def setear_grafo(trenes):
       w = math.ceil(s['demand'][0] / trenes['rs_info']['capacity'])
       src = (s['stops'][0]['time'], s['stops'][0]['station'])
       dst = (s['stops'][1]['time'], s['stops'][1]['station'])
-      edges.append((src, dst, {'lower_bound': w, 'upper_bound': trenes['rs_info']['max_rs'], 'weight': 0, 'color': 'green'}))
+      R.add_edge(src, dst, lower_bound=w, upper_bound=trenes['rs_info']['max_rs'], weight=0, color='green')
 
-   R.add_edges_from(edges)
-   
-   # sumo/resto demandas de servicios que salen/entran en cierto horario
+
+   # ajusto demandas {sumo/resto demandas de servicios que salen/entran en cierto horario}
    for s in trenes['services'].values():
       w = math.ceil(s['demand'][0] / trenes['rs_info']['capacity'])
       id_dep = (s['stops'][0]['time'], s['stops'][0]['station'])
       id_arr = (s['stops'][1]['time'], s['stops'][1]['station'])
       for n in R.nodes(data="True"):
          if n[0] == id_dep:
-            R.nodes[n[0]]['demand'] = R.nodes[n[0]]['demand'] + w
+            R.nodes[n[0]]['demand'] += w
          if n[0] == id_arr:
-            R.nodes[n[0]]['demand'] = R.nodes[n[0]]['demand'] - w
-      
+            R.nodes[n[0]]['demand'] -= w
+
+   # lista para aristas entre misma estacion; aristas azules y rojas. sé que están en orden creciente segun tiempo porque las creo a partir de services y services está ordenada
    services = list(R.nodes(data=True))
    services.sort(key=lambda x: (x[0][0]))
    station1 = []
    station2 = []
-   # lista para aristas entre misma estacion para aristas azules y rojas. sé que están en orden creciente segun tiempo porque las creo a partir de services y services
    for nodo in services:
       if nodo[0][1] == cabeceras[0]:
          station1.append(nodo)
@@ -56,7 +54,6 @@ def setear_grafo(trenes):
    # aristas azules
    for i in range(1, len(station1)):
       R.add_edge(station1[i-1][0], station1[i][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=0, color='blue')
-      
    for i in range(1, len(station2)):
       R.add_edge(station2[i-1][0], station2[i][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=0, color='blue')
    
@@ -104,7 +101,7 @@ def costo_min(G:nx.DiGraph):
 def main():
    # filename = "instances/toy_instance.json"
    filename = "instances/retiro-tigre-semana.json"
-   # filename = "instances/toy_tiempos_repetidos.json"
+   # filename = "instances/toy_repetidos.json
 
    with open(filename) as json_file:
       data = json.load(json_file)
