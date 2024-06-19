c52c0b207d01b1930a3af90b6e9183b2b4ee34dc
Catalina Chab López <catalinachablopez@Catalinas-MacBook-Air-6.local>
2024-06-18 21:38:45 -0300

    ejercicio 5

--- src/ejercicio_5.py
+++ src/ejercicio_5.py
@@ -4,7 +4,7 @@ import copy
 from math import *
 import math
 
-def setear_grafo(trenes):
+def setear_grafo_roto(trenes,u, rota:str):
    
    cabeceras = tuple(trenes['stations'])
    R = nx.DiGraph()
@@ -14,14 +14,13 @@ def setear_grafo(trenes):
       for i in range(2): # 0 <-> type D (departure)  ^  1 <-> type A (arrival)
          nodo_id = (s['stops'][i]['time'], s['stops'][i]['station'])
          if nodo_id not in R.nodes():
-            R.add_node(nodo_id, demand=0)
-               
+            R.add_node(nodo_id, demand=0)            
       # aristas verdes -> de tren
       l = math.ceil(s['demand'][0] / trenes['rs_info']['capacity'])
       src = (s['stops'][0]['time'], s['stops'][0]['station'])
       dst = (s['stops'][1]['time'], s['stops'][1]['station'])
       R.add_edge(src, dst, lower_bound=l, upper_bound=trenes['rs_info']['max_rs'], weight=0, color='green')
-
+   
    # ajusto demandas {sumo/resto demandas de servicios que salen/entran en cierto horario}
    for s in trenes['services'].values():
       d = math.ceil(s['demand'][0] / trenes['rs_info']['capacity'])
@@ -47,51 +46,33 @@ def setear_grafo(trenes):
    BIG_NUMBER = float(inf)
    
    # aristas rojas -> trasnoche
-   R.add_edge(station1[-1][0], station1[0][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=1, color='red')
-   R.add_edge(station2[-1][0], station2[0][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=1, color='red')
-   
+   if rota==cabeceras[0]:
+      R.add_edge(station1[-1][0], station1[0][0], lower_bound=0, upper_bound=u, weight=1, color='red')
+      R.add_edge(station2[-1][0], station2[0][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=1, color='red')
+      d = R.nodes[station1[0][0]]['demand']
+   else:
+      R.add_edge(station1[-1][0], station1[0][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=1, color='red')
+      R.add_edge(station2[-1][0], station2[0][0], lower_bound=0, upper_bound=u, weight=1, color='red')
+      d = R.nodes[station2[0][0]]['demand']
    # aristas azules -> traspaso
    for i in range(1, len(station1)):
       R.add_edge(station1[i-1][0], station1[i][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=0, color='blue')
    for i in range(1, len(station2)):
       R.add_edge(station2[i-1][0], station2[i][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=0, color='blue')
    
+   #agregamos el nodo z para q almacene las unidades adicionales de la cabecera rota
+   
+   R.add_node('z')
+
+   if rota==cabeceras[0]:
+      R.add_edge( station1[-1][0],'z', lower_bound=0, upper_bound=BIG_NUMBER, weight=2, color='blue')
+      R.add_edge('z',station1[0][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=2, color='blue')
+   else:
+      R.add_edge( station2[-1][0],'z', lower_bound=0, upper_bound=BIG_NUMBER, weight=2, color='blue')
+      R.add_edge('z',station2[0][0], lower_bound=0, upper_bound=BIG_NUMBER, weight=2, color='blue')
    return R
 
-def costo_min(G:nx.DiGraph):
-   R = copy.deepcopy(G)
-   
-   # redefino aristas
-   for e in R.edges(data=True):
-      if e[2]['color'] == 'green':
-         R[e[0]][e[1]]['upper_bound'] = R[e[0]][e[1]]['upper_bound'] - R[e[0]][e[1]]['lower_bound']
-         R[e[0]][e[1]]['lower_bound'] = 0
-
-   flowDict = nx.min_cost_flow(R, demand='demand', capacity='upper_bound', weight='weight')
-   
-   # calculo costo minimo sumando el flujo de las aristas de trasnoche (rojas)
-      # que es equivalente a hacer (flujo * peso) para cada arista, porque las demas aristas tienen peso 0
-   costo=0
-   for e in R.edges():
-      if e[0] == 'z'or e[1] == 'z':
-         print(f'flujo arista de {e[0]} a {e[1]}: {flowDict[e[0]][e[1]]}')
-         if e[0] == 'z':
-            costo += flowDict[e[0]][e[1]]
-      if R[e[0]][e[1]]['color'] == 'red':
-         print(f'flujo arista de {e[0]} a {e[1]}: {flowDict[e[0]][e[1]]}')
-         costo += flowDict[e[0]][e[1]]
-   return costo
 
 
-def costo_max(trenes):
-   #sería el costo sin reciclar servicios
-   d=0
-   for s in trenes['services'].values():
-      d+=math.ceil(s['demand'][0] / trenes['rs_info']['capacity'])
-   return d
-
-def plata_ahorrada(costo_min,costo_max, cost_vagon):
-   return(costo_max-costo_min)* cost_vagon
-   
 
 
--- src/main_alu.py
+++ src/main_alu.py
@@ -1,8 +1,9 @@
 import json
 from funciones import *
+from ejercicio_5 import *
    
 def main():
-   #filename = "instances/toy_instance.json"
+   filename = "instances/toy_instance.json"
    #filename = "instances/retiro-tigre-semana.json"
    # filename = "instances/toy_repetidos.json"
    # filename = "instances/moreno-once.json"
@@ -13,14 +14,18 @@ def main():
       data = json.load(json_file)
 
    # test file reading
-   R = setear_grafo(data)
+   # R = setear_grafo(data)
+   # costo_minimo = costo_min(R)
+   # print(f'costo_minimo: {costo_minimo}')
+   # costo_maximo = costo_max(data)
+   # print(f'costo_maximo: {costo_maximo}')
+   # #Tomamos data de un artículo de infobae 2018 donde se decía que cada vagón para el Roca costaban 1.580.000 dólares
+   # ahorro= plata_ahorrada(costo_minimo,costo_maximo, cost_vagon=1500000)
+   # print(f'plata_ahorrada: {ahorro}')
+   R = setear_grafo_roto(data, 20, 'Tigre')
    costo_minimo = costo_min(R)
    print(f'costo_minimo: {costo_minimo}')
-   costo_maximo = costo_max(data)
-   print(f'costo_maximo: {costo_maximo}')
-   #Tomamos data de un artículo de infobae 2018 donde se decía que cada vagón para el Roca costaban 1.580.000 dólares
-   ahorro= plata_ahorrada(costo_minimo,costo_maximo, cost_vagon=1500000)
-   print(f'plata_ahorrada: {ahorro}')
+   
 
 if __name__ == "__main__":
    main()
